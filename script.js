d7d1230a6e628a246b1660f6e34fc4a981dd6640
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-12 14:59:39 +0300

    complete exercise that consoles each time new numbers in lyrics

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let question = prompt("How many beers woul you like to see on the wall?");
 
 let numOfBeers = Number(question);
+let counter = 1;
 
+while (numOfBeers != 0) {
+    for (let i = 0; i < numOfBeers; i++) {
+        let pronoun;
+        if (counter == 1) {
+            pronoun = 'it'
+        } else {
+            pronoun = 'them';
+        }
+      let secondValue = numOfBeers;
+      console.log(`
+      ${numOfBeers--} bottles of beer on the wall
+      ${secondValue--} bottles of beer
+      Take ${counter++} down, pass ${pronoun} around
+      ${numOfBeers} bottles of beer on the wall`);
+  }
+}
